dd73af149a818e301f7dcc7b0f3388d7a8bfc6e4
Bill ZHONG <billzhonggz@outlook.com>
2018-04-08 17:00:32 +0800

    Initialize as PyCharm project. TCP server is now running.

--- tcp/models.py
+++ tcp/models.py
@@ -1,20 +1,28 @@
 from django.db import models
 from tornado.ioloop import IOLoop
 from tornado.tcpserver import TCPServer
+from tornado.iostream import StreamClosedError
+from tornado import gen
+
 
 # Create your models here.
 
 
 class IotTcpServer(TCPServer):
+    @gen.coroutine
     def handle_stream(self, stream, address):
-        def got_data(data):
-            print "Input: {}".format(repr(data))
-            stream.write("OK", stream.close)
-
-        stream.read_until("\n", got_data)
+        while True:
+            try:
+                data = yield stream.read_until(b"\n")
+                yield stream.write(data)
+                data_string = data.decode("utf-8")
+                print(data_string)
+            except StreamClosedError:
+                break
 
 
 if __name__ == '__main__':
     server = IotTcpServer()
     server.listen(9876)
-    IOLoop.instance().start()
+    print('TCP server is running.')
+    IOLoop.current().start()
