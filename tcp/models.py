4ccb016d7b8c2aff9cd57bca4b772cf2efadb34e
Bill ZHONG <billzhonggz@outlook.com>
2018-04-08 17:41:31 +0800

    Update ORM model and view.

--- tcp/models.py
+++ tcp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+
 from tornado.ioloop import IOLoop
 from tornado.tcpserver import TCPServer
 from tornado.iostream import StreamClosedError
@@ -17,10 +19,22 @@ class IotTcpServer(TCPServer):
                 yield stream.write(data)
                 data_string = data.decode("utf-8")
                 print(data_string)
+                # Write data to ORM.
+                RawData.set_raw_data(RawData, data_string)
             except StreamClosedError:
                 break
 
 
+class RawData(models.Model):
+    receive_date = models.DateField(auto_now_add=True, blank=True)
+    receive_time = models.TimeField(auto_now_add=True, blank=True)
+    raw_data = models.CharField(max_length=100)
+
+    def set_raw_data(self, data):
+        self.raw_data = data
+
+
+# TEMP: Test code for TCP server.
 if __name__ == '__main__':
     server = IotTcpServer()
     server.listen(9876)
--- tcp/views.py
+++ tcp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+
+def DisplayTcpReceive(request):
+    now = datetime.datetime.now()
+    html = "<html><body>Current time is %s. </body></html>" % now
+    # TODO: Set response as a template, insert the received data.
+    return HttpResponse(html)
